cb2855f06d1d6978e6ffedf867a4bd71f6f5557b
Lucas Tian <lucasytian@gmail.com>
2020-05-22 19:31:13 -0400

    minor

--- pythonlib/tools/snstools.py
+++ pythonlib/tools/snstools.py
@@ -15,3 +15,18 @@ def addLabel(ax):
     for a in ax.axes.flat:
         a.set_xticklabels(a.get_xticklabels(), rotation=45, 
             horizontalalignment="right")
+
+
+def addTextLabelToPoints(ax, df, x, y, name):
+    """ 
+    adds on top of points labels in text.
+    e.g., ax = sns.scatterplot(x="x", y="y", hue="category", data=df, hue_order=sorted(list(set(tasks_all_categories))))
+    addTextLabelToPoints(ax, df, "x", "y", "name"),
+    i.e,., tell me the x, y, and text strings to use 
+    """
+    
+    # add annotations one by one with a loop
+    for line in range(df.shape[0]):
+        ax.text(df[x][line], df[y][line], df[name][line], 
+                 horizontalalignment='left', size='medium', color='black',
+                alpha=0.6)
