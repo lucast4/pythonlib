45d6aa5afcf3f4ad9a16f557102aed4f6a9b7256
kedar <kd.gz.gp@gmail.com>
2023-01-18 13:38:38 -0500

    fix checktools bug

--- pythonlib/tools/checktools.py
+++ pythonlib/tools/checktools.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def check_objects_identical(obj1, obj2):
+def check_objects_identical(obj1, obj2, PRINT=False):
     """ Works with commonly used objects, checks if identical,
     Returns False if any difference found
     otherwise True.
@@ -8,34 +8,61 @@ def check_objects_identical(obj1, obj2):
 
     # Go thru all cases. if any are false, returns False
     if not type(obj1)==type(obj2):
+        if PRINT:
+            print('obj1',obj1)
+            print('obj2',obj2)
         return False
     elif isinstance(obj1, list):
         if not len(obj1)==len(obj2):
+            if PRINT:
+                print('obj1',obj1)
+                print('obj2',obj2)
             return False
         for x, y in zip(obj1, obj2):
             good = check_objects_identical(x,y)
             if not good:
+                if PRINT:
+                    print('obj1',obj1)
+                    print('obj2',obj2)
                 return False
     elif isinstance(obj1, dict):
         if not sorted(obj1.keys()) == sorted(obj2.keys()):
+            if PRINT:
+                print('obj1',obj1)
+                print('obj2',obj2)
             return False
         for k in obj1.keys():
             good = check_objects_identical(obj1[k], obj2[k])
             if not good:
+                if PRINT:
+                    print('obj1',obj1)
+                    print('obj2',obj2)
                 return False
     elif isinstance(obj1, np.ndarray):
-        if not np.all(np.isclose(obj1, obj2)):
+        if np.all(np.isnan(obj1)) and np.all(np.isnan(obj2)):
+            pass
+        elif not np.all(np.isclose(obj1, obj2)):
+            if PRINT:
+                print('obj1',obj1)
+                print('obj2',obj2)
             return False
+        else:
+            pass
     elif isinstance(obj1, (int, str)):
         if not obj1==obj2:
+            if PRINT:
+                print('obj1',obj1)
+                print('obj2',obj2)
             return False
     else:
-        print(obj1)
-        print(obj2)
+        print('obj1',obj1)
+        print('obj2',obj2)
         print(type(obj1))
         print(type(obj2))
         assert False, "add this type"
-
+    
+    if PRINT:
+        print("tests passed")
     # got here, passed all tests.
     return True
         
