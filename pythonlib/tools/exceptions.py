af9850823224a80994690d103de71b7e2200e3eb
Lucas <lucasytian@gmail.com>
2023-06-28 17:15:11 -0400

    adding an exception

--- pythonlib/tools/exceptions.py
+++ pythonlib/tools/exceptions.py
@@ -2,3 +2,9 @@ class NotEnoughDataException(Exception):
 	"""useful to skip some anakysis in context of a batch analys"""
 	pass
 		
+
+class DataMisalignError(Exception):
+	""" If there are weird misalingments in data
+	e.g, mapping neural to beh
+	"""
+	pass
