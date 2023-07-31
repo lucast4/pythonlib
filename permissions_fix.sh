da63097e55789698600746a80b8c65d68beba8bd
Lucas <lucasytian@gmail.com>
2023-07-31 09:18:25 -0400

    minor

---
+++ permissions_fix.sh
@@ -0,0 +1,13 @@
+
+sudo chmod -R u=rwx,g=rx,o=rx /gorilla1
+sudo chmod -R u=rwx,g=rx,o=rx /gorilla2
+sudo chmod -R u=rwx,g=rx,o=rx /gorilla2_backup
+sudo chmod -R u=rwx,g=rx,o=rx /gorilla3
+sudo chmod -R u=rwx,g=rx,o=rx /gorilla4
+
+sudo chown -R lucast4 /gorilla1
+sudo chown -R lucast4 /gorilla2
+sudo chown -R lucast4 /gorilla2_backup
+sudo chown -R lucast4 /gorilla3
+sudo chown -R lucast4 /gorilla4
+
