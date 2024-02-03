952baa249b694072ec4235958552d2cbc48a8320
Lucas <lucasytian@gmail.com>
2024-02-02 19:28:43 -0500

    minor

--- pythonlib/copy_things_from_server.sh
+++ pythonlib/copy_things_from_server.sh
@@ -1,12 +1,12 @@
 # To copy things from server (originally from bonobo) to gorilla.
 
 # analyses
-rsync --verbose -avz /mnt/hopfield_data01/ltian/backup/bonobo/data2/analyses/database /gorilla1/analyses/
+rsync --verbose -avz /mnt/Freiwald/ltian/backup/bonobo/data2/analyses/database /gorilla1/analyses/
 #rsync --verbose -avz /mnt/bonobo/data2/analyses/database/BEH /gorilla1/analyses/database/
 rsync --verbose -avz /mnt/bonobo/data2/analyses /gorilla1/
 
 # neural preprocess
-rsync --verbose -avz /mnt/hopfield_data01/ltian/backup/bonobo/data3/recordings /gorilla1/neural_preprocess/
+rsync --verbose -avz /mnt/Freiwald/ltian/backup/bonobo/data3/recordings /gorilla1/neural_preprocess/
 
 # monkeylogic pkl processed files
 # NOT WORKING
@@ -14,7 +14,7 @@ rsync --verbose -avz /mnt/hopfield_data01/ltian/backup/bonobo/data3/recordings /
 # rsync -rav0z --include '*/' --include '*.pkl' --exclude '*' /mnt/hopfield_data01/ltian/backup/bonobo/data2/animals/Pancho/ /gorilla1/test/
 
 # This filters to only take pkl, but doesnt filter by date...
-rsync -avz --include '*/' --include '*.pkl' --exclude '*' --prune-empty-dirs /mnt/hopfield_data01/ltian/backup/bonobo/data2/animals/Pancho /gorilla1/animals/
+rsync -avz --include '*/' --include '*.pkl' --exclude '*' --prune-empty-dirs /mnt/Freiwald/ltian/backup/bonobo/data2/animals/Pancho /gorilla1/animals/
 rsync -avz --include '*/' --include '*.pkl' --exclude '*' --prune-empty-dirs /mnt/bonobo/data2/animals/Pancho /gorilla1/animals/
 rsync -avz --include '*/' --include '*/figures/*' --include '*.pkl' --exclude '*' --prune-empty-dirs /mnt/bonobo/data2/animals/Pancho /gorilla1/animals/
 
