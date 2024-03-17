3d2a952fb4167e6835fe4fcd887870d8be489b87
Lucas <lucasytian@gmail.com>
2024-03-17 14:22:55 -0400

    k means clustering

--- pythonlib/tools/statstools.py
+++ pythonlib/tools/statstools.py
@@ -262,3 +262,157 @@ def _crossval_folds_indices_onsets(n1, n2, nfold=None):
             assert False
 
 
+def cluster_kmeans_with_silhouette_score(X, n_clusters=None, n_clusters_min_max=None, PLOT_SIL=False,
+                                         PLOT_FINAL=False, return_figs=False):
+    """
+    K-means clustering, with optinaly finding n clusters by maximizing silhoutte score
+    over range of possible n_clsuters.
+
+    Code taken from: https://scikit-learn.org/stable/auto_examples/cluster/plot_kmeans_silhouette_analysis.html#sphx-glr-auto-examples-cluster-plot-kmeans-silhouette-analysis-py
+    :param X:
+    :param n_clusters:
+    :param n_clusters_min_max: only needed if n_clusters is None, in which case shoudl be 2-list of int,
+    will try all n clusters in range of [min, max], and use the one that maximizes the silhoutte score.
+    :param PLOT:
+    :return: cluster_labels, list of ints
+    """
+    from sklearn.cluster import KMeans
+
+    if n_clusters is None:
+        assert isinstance(n_clusters_min_max, (list, tuple)) and len(n_clusters_min_max)==2
+        range_n_clusters = list(range(n_clusters_min_max[0], n_clusters_min_max[1]+1))
+
+        import matplotlib.cm as cm
+        from sklearn.metrics import silhouette_samples, silhouette_score
+
+        list_silhouette_avg = []
+        list_n_clusters = []
+        for n_clusters in range_n_clusters:
+
+            # Initialize the clusterer with n_clusters value and a random generator
+            # seed of 10 for reproducibility.
+            clusterer = KMeans(n_clusters=n_clusters, random_state=10)
+            cluster_labels = clusterer.fit_predict(X)
+
+            # The silhouette_score gives the average value for all the samples.
+            # This gives a perspective into the density and separation of the formed
+            # clusters
+            silhouette_avg = silhouette_score(X, cluster_labels)
+            print(
+                "For n_clusters =",
+                n_clusters,
+                "The average silhouette_score is :",
+                silhouette_avg,
+            )
+            list_silhouette_avg.append(silhouette_avg)
+            list_n_clusters.append(n_clusters)
+
+            if PLOT_SIL:
+                # Compute the silhouette scores for each sample
+                sample_silhouette_values = silhouette_samples(X, cluster_labels)
+
+                # Create a subplot with 1 row and 2 columns
+                fig, (ax1, ax2) = plt.subplots(1, 2)
+                fig.set_size_inches(18, 7)
+
+                # The 1st subplot is the silhouette plot
+                # The silhouette coefficient can range from -1, 1 but in this example all
+                # lie within [-0.1, 1]
+                ax1.set_xlim([-0.1, 1])
+                # The (n_clusters+1)*10 is for inserting blank space between silhouette
+                # plots of individual clusters, to demarcate them clearly.
+                ax1.set_ylim([0, len(X) + (n_clusters + 1) * 10])
+
+                y_lower = 10
+                for i in range(n_clusters):
+                    # Aggregate the silhouette scores for samples belonging to
+                    # cluster i, and sort them
+                    ith_cluster_silhouette_values = sample_silhouette_values[cluster_labels == i]
+
+                    ith_cluster_silhouette_values.sort()
+
+                    size_cluster_i = ith_cluster_silhouette_values.shape[0]
+                    y_upper = y_lower + size_cluster_i
+
+                    color = cm.nipy_spectral(float(i) / n_clusters)
+                    ax1.fill_betweenx(
+                        np.arange(y_lower, y_upper),
+                        0,
+                        ith_cluster_silhouette_values,
+                        facecolor=color,
+                        edgecolor=color,
+                        alpha=0.7,
+                    )
+
+                    # Label the silhouette plots with their cluster numbers at the middle
+                    ax1.text(-0.05, y_lower + 0.5 * size_cluster_i, str(i))
+
+                    # Compute the new y_lower for next plot
+                    y_lower = y_upper + 10  # 10 for the 0 samples
+
+                ax1.set_title("The silhouette plot for the various clusters.")
+                ax1.set_xlabel("The silhouette coefficient values")
+                ax1.set_ylabel("Cluster label")
+
+                # The vertical line for average silhouette score of all the values
+                ax1.axvline(x=silhouette_avg, color="red", linestyle="--")
+
+                ax1.set_yticks([])  # Clear the yaxis labels / ticks
+                ax1.set_xticks([-0.1, 0, 0.2, 0.4, 0.6, 0.8, 1])
+
+                # 2nd Plot showing the actual clusters formed
+                colors = cm.nipy_spectral(cluster_labels.astype(float) / n_clusters)
+                ax2.scatter(
+                    X[:, 0], X[:, 1], marker=".", s=30, lw=0, alpha=0.7, c=colors, edgecolor="k"
+                )
+
+                # Labeling the clusters
+                centers = clusterer.cluster_centers_
+                # Draw white circles at cluster centers
+                ax2.scatter(
+                    centers[:, 0],
+                    centers[:, 1],
+                    marker="o",
+                    c="white",
+                    alpha=1,
+                    s=200,
+                    edgecolor="k",
+                )
+
+                for i, c in enumerate(centers):
+                    ax2.scatter(c[0], c[1], marker="$%d$" % i, alpha=1, s=50, edgecolor="k")
+
+                ax2.set_title("The visualization of the clustered data.")
+                ax2.set_xlabel("Feature space for the 1st feature")
+                ax2.set_ylabel("Feature space for the 2nd feature")
+
+                plt.suptitle(
+                    "Silhouette analysis for KMeans clustering on sample data with n_clusters = %d"
+                    % n_clusters,
+                    fontsize=14,
+                    fontweight="bold",
+                )
+            else:
+                fig = None
+
+        n_clusters = list_n_clusters[np.argmax(list_silhouette_avg)]
+        print("Using n_clusters =", n_clusters)
+
+        clusterer = KMeans(n_clusters=n_clusters)
+        cluster_labels = clusterer.fit_predict(X)
+        if PLOT_FINAL:
+            fig_final, ax = plt.subplots()
+            for i in range(n_clusters):
+                ax.plot(X[cluster_labels==i, 0], X[cluster_labels==i, 1], "x", alpha=0.5, label=i)
+            # ax.scatter(X[:,0], X[:,1], c=cluster_labels, marker="x", alpha=0.5)
+            ax.set_title(f"FINAL CLUSTERS, n={n_clusters}")
+        else:
+            fig_final = None
+
+        # List of ints
+        cluster_labels = [int(c) for c in cluster_labels]
+
+        if return_figs:
+            return cluster_labels, fig, fig_final
+        else:
+            return cluster_labels
